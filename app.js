1cf984511e53dae116c0c1ac0fe2cb2a8c5325b9
Rajesh S <rajbca00@gmail.com>
2022-03-05 12:38:19 +0530

    done

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 var nodemailer = require('nodemailer');
+const prompt = require("prompt-sync")({ sigint: true });
+
+var email = prompt('Please enter your gmail: ');
+var password = prompt('Please enter your gmail password: ');
+
+var transponder = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:email,
+        pass: password
+    }
+});
+
+var mailOptions = {
+    from : email,
+    to: email,
+    subject:'From Node',
+    text:'Sending email from Nodemailer'
+};
+
+transponder.sendMail(mailOptions,function(err,info){
+    if(err) throw err;
+    console.log('mail sent')
+});
